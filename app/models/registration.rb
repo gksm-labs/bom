bf886c8cb635c921753998347f78fcea881b8caf
Jozef Steinhübl <contact@xhyrom.dev>
2026-03-10 11:07:59 +0100

    build: annotate models

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: registrations
+#
+#  id            :bigint           not null, primary key
+#  birth_date    :date             not null
+#  category      :string           not null
+#  city          :string           not null
+#  club          :string
+#  discipline    :string           not null
+#  email         :string           not null
+#  first_name    :string           not null
+#  gdpr_consent  :boolean          default(FALSE), not null
+#  gender        :string           not null
+#  last_name     :string           not null
+#  phone         :string           not null
+#  t_shirt_size  :string           not null
+#  terms_consent :boolean          default(FALSE), not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Registration < ApplicationRecord
   enum :gender, { male: "M", female: "F", unstated: "N" }, validate: true
   enum :discipline, { main_10km: "10km", hobby_5km: "5km" }, validate: true
